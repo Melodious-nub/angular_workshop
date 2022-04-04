27a2a7371d8868bb4490b7539d960a226471d977
Melodious-nub <suffixit003@gmail.com>
2022-04-04 13:32:48 +0600

    landing page working now

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css','../../../assets/css/style.css']
 })
 export class LandingComponent implements OnInit {
 
