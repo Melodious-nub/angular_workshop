ff03dc775369c600dd66f0340abb5203908685cf
Melodious-nub <suffixit003@gmail.com>
2022-06-06 15:17:13 +0600

    working on reset-pass ref. forgot pass

--- src/app/pages/reset-password/reset-password.component.ts
+++ src/app/pages/reset-password/reset-password.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ResetPasswordComponent implements OnInit {
 
-  constructor() { }
+  urlParam: any = {};
+
+  constructor(private route: ActivatedRoute,private api: DataService,private toastr: ToastrService, private router: Router,) { }
 
   ngOnInit(): void {
+    this.urlParam.data = this.route.snapshot.queryParamMap.get('token');
+    this.resetPass();
+  }
+
+  resetPass() {
+    this.api.forgotPassword(this.urlParam).subscribe(
+      res=>{
+        console.log('email',res, this.urlParam);
+        if(res.success === true){
+          this.toastr.success(res.message, 'Congratulation.');
+        } else {
+          this.toastr.warning(res.message, 'Oops!');
+        }
+      }
+    )
   }
 
 }
